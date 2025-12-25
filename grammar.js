0879d6f8b202b178142f8a084a0e0fe911898f61
Alfredo Menezes <amtrosul@hotmail.com>
2025-12-25 18:58:35 -0500

    TEST: Minimal builtin_variable with 5 items

--- grammar.js
+++ grammar.js
@@ -182,56 +182,8 @@ module.exports = grammar({
 
     identifier: $ => /[a-zA-Z_][a-zA-Z0-9_]*/,
 
-    // Split into multiple token() groups to avoid regex length limit in tree-sitter-wasm
-    builtin_variable: $ => choice(
-      // Non-A_ builtins
-      token(/Clipboard|ClipboardAll|ErrorLevel/),
-
-      // Script info
-      token(/A_ScriptDir|A_ScriptFullPath|A_ScriptName|A_ScriptHwnd/),
-      token(/A_LineNumber|A_LineFile|A_AhkVersion|A_AhkPath/),
-      token(/A_IsCompiled|A_IsUnicode|A_PtrSize/),
-
-      // Date/Time
-      token(/A_Now|A_NowUTC|A_TickCount/),
-      token(/A_Year|A_Mon|A_DD|A_MDay|A_WDay|A_YDay|A_YWeek/),
-      token(/A_Hour|A_Min|A_Sec|A_MSec/),
-
-      // Paths
-      token(/A_WorkingDir|A_Desktop|A_DesktopCommon|A_MyDocuments/),
-      token(/A_AppData|A_AppDataCommon|A_Temp/),
-      token(/A_StartMenu|A_Programs|A_Startup/),
-      token(/A_WinDir|A_ProgramFiles|A_ComSpec/),
-
-      // Screen/Display
-      token(/A_ScreenWidth|A_ScreenHeight|A_ScreenDPI/),
-
-      // Keyboard/Mouse
-      token(/A_Cursor|A_CaretX|A_CaretY|A_TimeIdle|A_TimeIdlePhysical/),
-
-      // Hotkey
-      token(/A_ThisHotkey|A_PriorHotkey|A_EndChar/),
-      token(/A_TimeSinceThisHotkey|A_TimeSincePriorHotkey/),
-
-      // Loop
-      token(/A_Index|A_LoopField|A_LoopReadLine/),
-      token(/A_LoopFileName|A_LoopFileFullPath|A_LoopFilePath|A_LoopFileDir|A_LoopFileExt/),
-      token(/A_LoopFileTimeCreated|A_LoopFileTimeModified|A_LoopFileTimeAccessed|A_LoopFileSize/),
-      token(/A_LoopRegName|A_LoopRegKey/),
-
-      // GUI
-      token(/A_Gui|A_GuiControl|A_GuiEvent|A_EventInfo/),
-      token(/A_GuiWidth|A_GuiHeight|A_GuiX|A_GuiY/),
-
-      // Other
-      token(/A_Space|A_Tab/),
-      token(/A_UserName|A_ComputerName/),
-      token(/A_IPAddress1|A_IPAddress2|A_IPAddress3|A_IPAddress4/),
-      token(/A_IsAdmin|A_OSType|A_OSVersion|A_Language/),
-      token(/A_IsSuspended|A_IsPaused|A_IsCritical/),
-      token(/A_BatchLines|A_TitleMatchMode|A_DetectHiddenWindows|A_DetectHiddenText/),
-      token(/A_LastError/),
-    ),
+    // MINIMAL TEST - just a few built-in variables to test if rule works at all
+    builtin_variable: $ => token(/A_ScriptDir|A_Now|A_TickCount|Clipboard|ErrorLevel/),
 
     // Remaining punctuation (braces, brackets, parens, dots, commas)
     _punctuation: $ => /[(){}\[\].,@$\\]+/,
