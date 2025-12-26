5d52f9a68f3c34b52d08d3605acd3c007d1812a8
Alfredo Menezes <amtrosul@hotmail.com>
2025-12-26 00:15:46 -0500

    feat: Add outline.scm for document symbols
    Enable document outline panel and go-to-symbol (Ctrl+Shift+O) for:
    - Functions (function_definition)
    - Labels (label)
    - Hotkeys (hotkey)
    - Directives (directive with # prefix)

---
+++ languages/autohotkey/outline.scm
@@ -0,0 +1,18 @@
+; Doc comments annotate the following item
+(doc_comment) @annotation
+
+; Function definitions
+(function_definition
+  name: (identifier) @name) @item
+
+; Labels (like MyLabel:)
+(label
+  name: (identifier) @name) @item
+
+; Hotkeys (like ^a::)
+(hotkey) @name @item
+
+; Directives (like #NoEnv)
+(directive
+  "#" @context
+  (identifier) @name) @item
